3679c7a73a2419f9b3459ac38db2c967ec865538
Matthew McCartney <25149329+mattmccartneyuk@users.noreply.github.com>
2026-03-08 20:45:37 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace InteractiveCalculator;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to interactive calculator");
+    }
+}
