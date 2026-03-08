3907cbd3310386c8e0e0fdeb5b888546b11b0423
Matthew McCartney <25149329+mattmccartneyuk@users.noreply.github.com>
2026-03-08 21:00:13 +0000

    Adding starting point of Raylib application

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
-﻿namespace InteractiveCalculator;
+﻿using Raylib_cs;
+
+namespace InteractiveCalculator;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to interactive calculator");
+        Raylib.InitWindow(800, 480, "InteractiveCalculator");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.LightGray);
+
+            Raylib.DrawText("Welcome to interactive calculator", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
     }
 }
